4ad0fabf7114ab6f775377ef9bf6305d2e3ea91d
mo-arman <moharman3818@gmail.com>
2025-01-15 23:28:15 +0530

    Add

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div",{});
 
-console.log(parent);
+const Header = () => {
+    return(
+        <div className="header">
+           <div className="logo">
+            <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3q8vyYK6Oo5FCSKCUraI09Ug0iqfLTg5hrQ&s" alt="logo-img"/>
+           </div>
+           <div className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+          </ul>
+           </div>
+        </div>
+    )
+}
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header/>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<AppLayout/>);
 
 
 
